4b52548c33f6da8e47b87ca05c2acd707f91b291
Henrique Mamede <henrique.silva@venturus.org.br>
2025-03-29 19:08:49 -0300

    feat: ✨ adiciona novo componente Layout

---
+++ src/app/AppLayout/components/Layout/LayoutHooks.ts
@@ -0,0 +1,52 @@
+import { ApplicationRoutes } from "@/enums/ApplicationRoutesEnums";
+
+export const useGetLayoutNavigationItems = () => {
+  const moduleOne = {
+    link: ApplicationRoutes.MODULE_ONE,
+    label: "Módulo 1",
+  };
+
+  const moduleTwo = {
+    link: ApplicationRoutes.MODULE_TWO,
+    label: "Módulo 2",
+  };
+
+  const moduleThree = {
+    link: ApplicationRoutes.MODULE_THREE,
+    label: "Módulo 3",
+  };
+
+  const moduleFour = {
+    link: ApplicationRoutes.MODULE_FOUR,
+    label: "Módulo 4",
+  };
+
+  const moduleFive = {
+    link: ApplicationRoutes.MODULE_FIVE,
+    label: "Módulo 5",
+  };
+
+  const moduleSix = {
+    link: ApplicationRoutes.MODULE_SIX,
+    label: "Módulo 6",
+  };
+
+  const moduleSeven = {
+    link: ApplicationRoutes.MODULE_SEVEN,
+    label: "Módulo 7",
+  };
+
+  const navigationItems = [];
+
+  navigationItems.push(
+    moduleOne,
+    moduleTwo,
+    moduleThree,
+    moduleFour,
+    moduleFive,
+    moduleSix,
+    moduleSeven
+  );
+
+  return navigationItems;
+};
