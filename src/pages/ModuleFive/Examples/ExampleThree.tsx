7b81fe31d89b2bdd032374d96ada527919c93810
Michelle Freitas <michelle.freitas@venturus.org.br>
2025-04-10 18:18:38 -0300

    fix: título e layout do exemplo 3

--- src/pages/ModuleFive/Examples/ExampleThree.tsx
+++ src/pages/ModuleFive/Examples/ExampleThree.tsx
@@ -11,10 +11,15 @@ function FormularioUseController() {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen gap-4">
-      {/* Componente de input personalizado controlado pelo React Hook Form */}    
-      <InputPersonalizado />
-      <Button type="submit" className="cursor-pointer">Enviar</Button>
+    <div className="flex items-center justify-center h-screen">
+      <div
+        className="flex flex-col gap-3 items-center justify-center w-[500px] p-5 border-2 border-solid border-gray-100 rounded-2xl"
+      >
+        <h1 className="text-xl text-neutral-800">Formulário com useController</h1>
+        {/* Componente de input personalizado controlado pelo React Hook Form */}
+        <InputPersonalizado />
+        <Button type="submit" className="cursor-pointer">Enviar</Button>
+      </div>
     </div>
   );
 }
