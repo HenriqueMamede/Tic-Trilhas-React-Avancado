74a99d4e58e41eef4c19dd0e4fbb3ec5a23374bf
Henrique Mamede <henrique.silva@venturus.org.br>
2025-05-07 14:47:55 -0300

    feat: implementa a tela base do modulo seis

--- src/pages/Modulo6/ModuloSeis.tsx
+++ src/pages/Modulo6/ModuloSeis.tsx
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+
 const ModuloSeis = () => {
-  return <div>Modulo Seis</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-20">
+      <h1 className="text-3xl">Exemplo de Portals</h1>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => setIsModalOpen(true)}
+      >
+        Abrir Modal
+      </button>
+      {isModalOpen && (
+        <div>
+          /* adicionar modal */
+        </div>
+      )}
+    </div>
+    );
 };
+
 
 export default ModuloSeis;
