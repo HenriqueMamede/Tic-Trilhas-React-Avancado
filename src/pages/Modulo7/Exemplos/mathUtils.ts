f193f82e8447895a6fc41cfeca0c49585f6bda06
Ketile Santos <ketile.santos@venturus.org.br>
2025-05-13 08:29:35 -0300

    Ajustes gerais no modulo sete

--- src/pages/Modulo7/Exemplos/mathUtils.ts
+++ src/pages/Modulo7/Exemplos/mathUtils.ts
@@ -1,7 +1,7 @@
 export const somar = (a: number, b: number): number => {
-  return a + b;
+    return a + b;
 };
 
 export const subtrair = (a: number, b: number): number => {
-  return a - b;
+    return a - b;
 };
