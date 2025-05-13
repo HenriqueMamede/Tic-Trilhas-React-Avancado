4b98ea03a0a79ed96deda26c9ea4a4cce061ffda
Ketile Santos <ketile.santos@venturus.org.br>
2025-05-13 08:29:19 -0300

    Ajustes gerais no modulo seis

--- src/pages/Modulo6/Exemplos/Exemplo1.tsx
+++ src/pages/Modulo6/Exemplos/Exemplo1.tsx
@@ -1,18 +1,19 @@
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
-import { useState } from 'react';
-import { Button } from '@/components/ui/button';
-import Modal from './components/Modal';
+const Exemplo1 = () => {
+    const [modalAberto, setModalAberto] = useState(false);
 
-function Exemplo1() {
-const [modalAberto, setModalAberto] = useState(false);
-
- return (
-    <div className="flex flex-col items-center justify-center h-screen gap-20">
-        <h1 className="text-3xl">Exemplo de Portals</h1>
-        <Button  onClick={() => setModalAberto(true)} className="cursor-pointer">Abrir Modal</Button>
-        {/* // TODO */}
-    </div>
- );
-}
+    return (
+        <div className="flex flex-col items-center justify-center h-screen gap-20">
+            <h1 className="text-3xl">Exemplo de Portals</h1>
+            <Button
+                onClick={() => setModalAberto(true)}
+                className="cursor-pointer">
+                Abrir Modal
+            </Button>
+        </div>
+    );
+};
 
 export default Exemplo1;
