80a3509f69b4699007b87219e7634dea2ec2c9df
Ketile Santos <ketile.santos@venturus.org.br>
2025-05-13 08:28:33 -0300

    Ajustes gerais no modulo três

--- src/pages/Modulo3/Exemplos/Exemplo1.tsx
+++ src/pages/Modulo3/Exemplos/Exemplo1.tsx
@@ -1,15 +1,15 @@
 import ComponenteDemorado from "@/pages/Modulo3/components/ComponenteDemorado";
 
-function ExemploUm() {
-  return (
-    <div className='flex flex-col gap-4 items-center justify-center h-screen'>
-      <h1 className='text-2xl'>Exemplo sem React.lazy</h1>
+const Exemplo1 = () => {
+    return (
+        <div className="flex flex-col gap-4 items-center justify-center h-screen">
+            <h1 className="text-2xl">Exemplo sem React.lazy</h1>
 
-      <div className='p-4 bg-gray-300 rounded-lg'>
-        <ComponenteDemorado />
-      </div>
-    </div>
-  );
-}
+            <div className="p-4 bg-gray-300 rounded-lg">
+                <ComponenteDemorado />
+            </div>
+        </div>
+    );
+};
 
-export default ExemploUm;
+export default Exemplo1;
