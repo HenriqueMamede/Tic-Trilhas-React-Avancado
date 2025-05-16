6d8d1b4ab7beccef41cc54d3040cfca9c6d5c1f1
Douglas Marcolino Pereira <douglas.pereira@venturus.org.br>
2025-05-16 11:19:46 -0300

    aula 5.6

--- src/pages/Modulo5/Exemplos/Exemplo4.tsx
+++ src/pages/Modulo5/Exemplos/Exemplo4.tsx
@@ -1,44 +1,100 @@
+import * as Yup from "yup";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 interface Participante {
     nome: string;
 }
 
+interface IDadosFormulario {
+    participantes: Participante[];
+}
+
+const schema = Yup.object({
+    participantes: Yup.array()
+        .of(
+            Yup.object({
+                nome: Yup.string()
+                    .min(3, "Nome muito pequeno")
+                    .required("Nome é obrigatório")
+            })
+        )
+        .required()
+});
+
 const Exemplo4 = () => {
     const [participantes, setParticipantes] = useState<Participante[]>([]);
+    const {
+        register,
+        control,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<IDadosFormulario>({
+        resolver: yupResolver(schema)
+    });
+
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: "participantes"
+    });
+
+    const onSubmit = (dadosDoFormulario: IDadosFormulario) => {
+        console.log(dadosDoFormulario);
+        setParticipantes(dadosDoFormulario.participantes);
+    };
 
     return (
         <div className="flex flex-col items-center justify-center h-screen gap-4">
-            <div className="flex flex-col gap-3 items-center justify-center w-[500px] p-5 border-2 border-solid border-gray-100 rounded-2xl">
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="flex flex-col gap-3 items-center justify-center w-[500px] p-5 border-2 border-solid border-gray-100 rounded-2xl">
                 <h1 className="text-xl text-neutral-800">
                     Cadastro de Participantes
                 </h1>
-                <div className="flex flex-col gap-2 w-[300px]">
-                    <Label htmlFor="nome">
-                        Nome <span className="text-red-500">*</span>
-                    </Label>
-                    <div className="flex gap-4">
-                        <div>
-                            <Input id="nome" />
-                            <p className="text-red-500 text-sm">
-                                mensagem de erro
-                            </p>
+                {fields.map((field, index) => (
+                    <div
+                        key={field.id}
+                        className="flex flex-col gap-2 w-[300px]">
+                        <Label htmlFor={`participantes.${index}.nome`}>
+                            Nome <span className="text-red-500">*</span>
+                        </Label>
+                        <div className="flex gap-4">
+                            <div>
+                                <Input
+                                    id={`participantes.${index}.nome`}
+                                    {...register(`participantes.${index}.nome`)}
+                                />
+                                <p className="text-red-500 text-sm">
+                                    {
+                                        errors.participantes?.[index]?.nome
+                                            ?.message
+                                    }
+                                </p>
+                            </div>
+                            <Button
+                                onClick={() => remove(index)}
+                                type="button"
+                                variant="outline">
+                                Remover
+                            </Button>
                         </div>
-                        <Button type="button" variant="outline">
-                            Remover
-                        </Button>
                     </div>
-                </div>
-                <Button type="button" className="w-50 cursor-pointer">
+                ))}
+
+                <Button
+                    onClick={() => append({ nome: "" })}
+                    type="button"
+                    className="w-50 cursor-pointer">
                     Adicionar participante
                 </Button>
                 <Button type="submit" className="w-50 cursor-pointer">
                     Cadastrar participantes
                 </Button>
-            </div>
+            </form>
             {participantes.length > 0 && (
                 <div className="flex flex-col gap-3 items-center justify-center w-[500px] p-5">
                     <h2>Participantes cadastrados:</h2>
