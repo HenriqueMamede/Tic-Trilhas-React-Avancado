efe85045f5c57adb93d3eb2344d42aa2dbf4cb2c
Henrique Mamede <henrique.silva@venturus.org.br>
2025-06-02 11:39:04 -0300

    refactor: removendo codigo a ser codado

--- src/pages/Modulo1/ModuloUm.tsx
+++ src/pages/Modulo1/ModuloUm.tsx
@@ -10,47 +10,6 @@ const ModuloUm = () => {
     const [user, setUser] = useState<User | null>(null);
     const intervalRef = useRef<NodeJS.Timeout | null>(null); // Definindo o tipo de intervalRef
 
-    // Simula componentDidMount
-    useEffect(() => {
-        console.log("[Mount] Buscando dados do usuário...");
-        fetchUser();
-
-        // Simula componentWillUnmount
-        return () => {
-            console.log("[Unmount]");
-            if (intervalRef.current) {
-                console.log("[Unmount] Limpando intervalo");
-                clearInterval(intervalRef.current);
-            }
-        };
-    }, []);
-
-    // Simula componentDidUpdate para quando user.name muda
-    useEffect(() => {
-        if (user?.name) {
-            document.title = `Bem-vindo, ${user.name}`;
-            console.log("[Update] Título do documento atualizado");
-        }
-    }, [user?.name]);
-
-    // useLayoutEffect antes da pintura do DOM
-    useLayoutEffect(() => {
-        console.log("[LayoutEffect] Antes da pintura do DOM");
-    });
-
-    // Simula chamada de API
-    const fetchUser = async () => {
-        const mockUser = await new Promise<User>((resolve) =>
-            setTimeout(() => resolve({ name: "Laís", age: 29 }), 1000)
-        );
-        setUser(mockUser);
-
-        // Começa um intervalo qualquer
-        intervalRef.current = setInterval(() => {
-            //  console.log(`[Interval] Usuário atual: ${mockUser.name}`);
-        }, 3000);
-    };
-
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-[#fafafa] to-[#6EB8E2] p-6">
             <div className="text-center max-w-2xl p-8 bg-opacity-80 rounded-lg shadow-xl bg-white">
