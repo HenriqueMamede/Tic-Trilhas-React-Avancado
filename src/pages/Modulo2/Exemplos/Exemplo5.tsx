76d77903b7ca02976707a3d2f69c2d5d3802874d
Douglas Marcolino Pereira <douglas.pereira@venturus.org.br>
2025-06-12 16:08:59 -0300

    feat: add example 5 solutions

--- src/pages/Modulo2/Exemplos/Exemplo5.tsx
+++ src/pages/Modulo2/Exemplos/Exemplo5.tsx
@@ -1,18 +1,32 @@
-import InputCustomizado from "@/pages/Modulo2/Exemplos/components/InputCustomizado.tsx";
+import { useRef } from "react";
+import InputCustomizado, {
+    InputCustomizadoHandle
+} from "@/pages/Modulo2/Exemplos/components/InputCustomizado.tsx";
 import { Button } from "@/components/ui/button.tsx";
 
 const Exemplo5 = () => {
+    const inputRef = useRef<InputCustomizadoHandle>(null);
+
     return (
         <div className="flex flex-col gap-8 items-center justify-center h-screen">
             <div className="p-4 bg-gray-100 rounded-lg">
                 <InputCustomizado
+                    ref={inputRef}
                     className="border border-gray-400 rounded p-1"
                     placeholder="Digite aqui..."
                 />
             </div>
             <div className="flex flex-col gap-2">
-                <Button className="cursor-pointer">Focar no Input</Button>
-                <Button className="cursor-pointer">Limpar o Input</Button>
+                <Button
+                    className="cursor-pointer"
+                    onClick={() => inputRef.current?.focusInput()}>
+                    Focar no Input
+                </Button>
+                <Button
+                    className="cursor-pointer"
+                    onClick={() => inputRef.current?.limparInput()}>
+                    Limpar o Input
+                </Button>
             </div>
         </div>
     );
